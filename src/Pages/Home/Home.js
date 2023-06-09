83a1efc587bbfedf68671cfc885ae3322c2ecbd0
jansenaulia <222011369@stis.ac.id>
2023-06-09 20:32:49 +0700

    add twitter

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { TwitterTweetEmbed } from "react-twitter-embed";
 import { Berita } from "../../Components/Berita";
 import { Hero } from "./Hero";
 
@@ -11,6 +12,14 @@ export const Home = () => {
           Berita Terbaru
         </h2>
         <Berita />
+        <h2 className="mt-12 mb-6 text-2xl font-bold text-primary font-heading">
+          Cuitan
+        </h2>
+        <div className="grid gap-12 grid-cols-1 md:grid-cols-3 ">
+          <TwitterTweetEmbed tweetId="1667010400217600000" />
+          <TwitterTweetEmbed tweetId="1666980949438115840" />
+          <TwitterTweetEmbed tweetId="1666833953872035840" />
+        </div>
       </div>
     </div>
   );
