d85a79d104e8d6426922694caae210796ab62fea
jansenaulia <222011369@stis.ac.id>
2023-06-17 11:38:07 +0700

    link

--- src/Pages/Berita/Beritagrid.js
+++ src/Pages/Berita/Beritagrid.js
@@ -5,65 +5,71 @@ export const Beritagrid = () => {
     {
       id: 1,
       img: "/images/anak.jpg",
-      headline: "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
+      headline:
+        "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
       tanggal: "11 Juni 2023",
-      link: "/berita/detail1",
+      link: "/berita/1",
     },
     {
       id: 2,
       img: "/images/hipertensi.jpg",
-      headline: "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
+      headline:
+        "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
       tanggal: "10 Juni 2023",
-      link: "/berita/detail2",
+      link: "/berita/2",
     },
     {
       id: 3,
       img: "/images/tbc.png",
       headline: "Deteksi TBC Capai Rekor Tertinggi di Tahun 2022",
       tanggal: "9 Juni 2023",
-      link: "/berita/detail3",
+      link: "/berita/3",
     },
     {
       id: 4,
       img: "/images/tbc.png",
       headline: "Deteksi TBC Capai Rekor Tertinggi di Tahun 2022",
       tanggal: "9 Juni 2023",
-      link: "/berita/detail3",
+      link: "/berita/3",
     },
     {
       id: 5,
       img: "/images/anak.jpg",
-      headline: "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
+      headline:
+        "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
       tanggal: "11 Juni 2023",
-      link: "/berita/detail1",
+      link: "/berita/1",
     },
     {
       id: 6,
       img: "/images/hipertensi.jpg",
-      headline: "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
+      headline:
+        "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
       tanggal: "10 Juni 2023",
-      link: "/berita/detail2",
+      link: "/berita/2",
     },
     {
       id: 7,
       img: "/images/hipertensi.jpg",
-      headline: "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
+      headline:
+        "Hipertensi Disebut sebagai Silent Killer, Menkes Budi Imbau Rutin Cek Tekanan Darah",
       tanggal: "10 Juni 2023",
-      link: "/berita/detail2",
+      link: "/berita/2",
     },
     {
       id: 8,
       img: "/images/tbc.png",
       headline: "Deteksi TBC Capai Rekor Tertinggi di Tahun 2022",
       tanggal: "9 Juni 2023",
-      link: "/berita/detail3",
+      link: "/berita/3",
     },
     {
       id: 9,
       img: "/images/anak.jpg",
-      headline: "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
-      tanggal: "/berita/detail1",
-      link: "/berita/detail1",
+      headline:
+        "Kasus Gagal Ginjal Akut Pada Anak Meningkat, Orang Tua Diminta Waspada",
+      tanggal: "11 Juni 2023",
+      link: "/berita/1",
     },
   ];
   return (
