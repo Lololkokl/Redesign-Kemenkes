7f62d22ece94eff4836df0a1450bc90ecdb2917d
jansenaulia <222011369@stis.ac.id>
2023-06-17 15:05:39 +0700

    searchbar berita

--- src/Pages/Berita/Beritapage.js
+++ src/Pages/Berita/Beritapage.js
@@ -1,13 +1,14 @@
 import { Pagetitle } from "../../Components/Pagetitle";
 import { Pagination } from "../../Components/Pagination";
 import { Searchbar } from "../../Components/Searchbar";
+import { SearchbarBerita } from "../../Components/SearchbarBerita";
 import { Beritagrid } from "./Beritagrid";
 
 export const Beritapage = () => {
   return (
     <div className="mx-16 md:mx-48 mt-12 mb-24">
       <Pagetitle text="Berita" />
-      <Searchbar placeholder="Cari Berita" />
+      <SearchbarBerita placeholder="Cari Berita" />
       <Beritagrid />
       <Pagination />
     </div>
